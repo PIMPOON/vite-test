9553d7e94eca65d5c3761ffb99e997d8306bc3eb
Pier-Olivier Létourneau <111366978+PIMPOON@users.noreply.github.com>
2025-10-23 18:52:11 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/vite-test/',
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Default output directory
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
+});
